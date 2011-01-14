01luna/CHDK

commit 0c59933b61a547d6353f8e38d0d4ba59ef53bae4

clean up key handling some more, fix broken MD in lua, make ubasic_test build again.
git-svn-id: https://subversion.assembla.com/svn/chdk/trunk@1036 6794e30b-3f2a-0410-a806-a2bbca1c07ff

lib/ubasic/ubasic.c
@@ -46,6 +46,7 @@
 #include <fcntl.h>
 #include <io.h>
 #include <stdlib.h> /* rand,srand */
+#include "camera_functions.h"
 #else
 #include "ubasic.h"
 #include "platform.h"
@@ -55,13 +56,14 @@
 #include "stdlib.h"
 #include "levent.h"
 #include "console.h"
+#include "../../core/motion_detector.h"
 #endif
+#include "../../core/action_stack.h"
 #include "tokenizer.h"
 
 
 #include "../../include/conf.h"
 
-#include "camera_functions.h"
 
 
 #define INCLUDE_OLD_GET__SYNTAX
@@ -161,6 +163,18 @@ ubasic_init(const char *program)
   ubasic_error = UBASIC_E_NONE;
 }
 /*---------------------------------------------------------------------------*/
+// read a key name and return key id, 
+// set error and return 0 if key invalid
+static int ubasic_get_key_arg() {
+  int k;
+  tokenizer_string(string, sizeof(string));
+  tokenizer_next();
+  k = script_keyid_by_name(string);
+  if (k <= 0)
+    ubasic_error = UBASIC_E_UNK_KEY;
+  return k;
+}
+/*---------------------------------------------------------------------------*/
 static void
 accept(int token)
 {
@@ -251,9 +265,7 @@ case TOKENIZER_GET_VBATT:
     break;
  case TOKENIZER_IS_KEY:
     accept(TOKENIZER_IS_KEY);
-    tokenizer_string(string, sizeof(string));
-    tokenizer_next();
-    r = camera_is_clicked(string);
+    r = script_key_is_clicked(ubasic_get_key_arg());
     break;
 case TOKENIZER_SCRIPT_AUTOSTARTED:
     accept(TOKENIZER_SCRIPT_AUTOSTARTED);
@@ -277,16 +289,16 @@ case TOKENIZER_GET_EXP_COUNT:
     break;
 case TOKENIZER_IS_PRESSED:
     accept(TOKENIZER_IS_PRESSED);
-    tokenizer_string(string, sizeof(string));
-    tokenizer_next();
-    r = camera_is_pressed(string);
+    r = script_key_is_pressed(ubasic_get_key_arg());
     break;
   case TOKENIZER_RANDOM:
     accept(TOKENIZER_RANDOM);
     int min = expr();
     int max = expr();
+    // shouldn't srand every time...
     srand((int)shooting_get_bv96()+(unsigned short)stat_get_vbatt()+get_tick_count());
-    camera_sleep(rand()%10);
+    // wtf
+    action_push_delay(rand()%10);
     r = min + rand()%(max-min+1);
   break;
   case TOKENIZER_GET_MOVIE_STATUS:
@@ -1421,10 +1433,10 @@ end_statement(void)
 static void
 click_statement(void)
 {
-  accept(TOKENIZER_CLICK);
-  tokenizer_string(string, sizeof(string));
-  camera_click(string);
-  tokenizer_next();
+  int k = ubasic_get_key_arg();
+  if (k > 0)
+    action_push_click(k);
+
   DEBUG_PRINTF("End of click\n");
   accept_cr();
 }
@@ -1432,10 +1444,9 @@ click_statement(void)
 static void
 press_statement(void)
 {
-  accept(TOKENIZER_PRESS);
-  tokenizer_string(string, sizeof(string));
-  camera_press(string);
-  tokenizer_next();
+  int k = ubasic_get_key_arg();
+  if (k > 0)
+    action_push_press(k);
   DEBUG_PRINTF("End of press\n");
   accept_cr();
 }
@@ -1443,10 +1454,9 @@ press_statement(void)
 static void
 release_statement(void)
 {
-  accept(TOKENIZER_RELEASE);
-  tokenizer_string(string, sizeof(string));
-  camera_release(string);
-  tokenizer_next();
+  int k = ubasic_get_key_arg();
+  if (k > 0)
+    action_push_release(k);
   DEBUG_PRINTF("End of release\n");
   accept_cr();
 }
@@ -1457,7 +1467,7 @@ sleep_statement(void)
   int val;
   accept(TOKENIZER_SLEEP);
   val = expr();
-  camera_sleep(val);
+  action_push_delay(val);
   DEBUG_PRINTF("End of sleep\n");
   accept_cr();
 }
@@ -1466,7 +1476,7 @@ static void
 shoot_statement(void)
 {
   accept(TOKENIZER_SHOOT);
-  camera_shoot();
+  action_push(AS_SHOOT);
   DEBUG_PRINTF("End of shoot\n");
   accept_cr();
 }
@@ -2114,7 +2124,7 @@ static void wait_click_statement()
         tokenizer_token() != TOKENIZER_ELSE ) {
         timeout = expr();
     }
-    camera_wait_click(timeout);
+    action_wait_for_click(timeout);
     accept_cr();
 }
 
@@ -2124,9 +2134,7 @@ static void is_key_statement(void)
     accept(TOKENIZER_IS_KEY);
     var = tokenizer_variable_num();
     accept(TOKENIZER_VARIABLE);
-    tokenizer_string(string, sizeof(string));
-    tokenizer_next();
-    ubasic_set_variable(var, camera_is_clicked(string));
+    ubasic_set_variable(var, script_key_is_clicked(ubasic_get_key_arg()));
     DEBUG_PRINTF("End of is_key\n");
     accept_cr();
 }
@@ -2710,6 +2718,7 @@ line_statement(void)
   statement();
   return;
 }
+
 /*---------------------------------------------------------------------------*/
 void
 ubasic_run(void)

core/kbd.c
@@ -941,47 +941,6 @@ long kbd_process()
     return kbd_blocked;
 }
 
-static const struct Keynames {
-    int keyid;
-    char *keyname;
-} keynames[] = {
-    { KEY_UP,           "up"         },
-    { KEY_DOWN,         "down"       },
-    { KEY_LEFT,         "left"       },
-    { KEY_RIGHT,        "right"      },
-    { KEY_SET,          "set"        },
-    { KEY_SHOOT_HALF,   "shoot_half" },
-    { KEY_SHOOT_FULL,   "shoot_full" },
-    { KEY_ZOOM_IN,      "zoom_in"    },
-    { KEY_ZOOM_OUT,     "zoom_out"   },
-    { KEY_MENU,         "menu"       },
-    { KEY_DISPLAY,      "display"    },
-    { KEY_PRINT,        "print"      },
-    { KEY_ERASE,        "erase"      },
-    { KEY_ISO,          "iso"        },
-    { KEY_FLASH,        "flash"      },
-    { KEY_MF,           "mf"         },
-    { KEY_MACRO,        "macro"      },
-    { KEY_VIDEO,        "video"      },
-    { KEY_TIMER,        "timer"      },
-    { KEY_EXPO_CORR,    "expo_corr"  },
-    { KEY_MICROPHONE,   "fe"         },
-    { KEY_ZOOM_ASSIST,  "zoom_assist"},
-    { KEY_AE_LOCK,      "ae_lock"    },
-    { KEY_METERING,     "metering_mode"},
-    { 0xFF,             "remote"     },
-	{ 0xFFFF,           "no_key"     },
-};
-
-int keyid_by_name (const char *n)
-{
-    int i;
-    for (i=0;i<sizeof(keynames)/sizeof(keynames[0]);i++)
-	if (strcmp(keynames[i].keyname,n) == 0)
-	    return keynames[i].keyid;
-    return 0;
-}
-
 int kbd_is_blocked() {
 	return kbd_blocked;
 }

core/luascript.c
@@ -1,5 +1,4 @@
 #include "luascript.h"
-#include "../lib/ubasic/camera_functions.h"
 #include "kbd.h"
 #include "platform.h"
 #include "script.h"
@@ -14,8 +13,9 @@
 #include "levent.h"
 #include "console.h"
 #include "action_stack.h"
+#include "motion_detector.h"
 
-#include "../lib/lua/lstate.h"	// for L->nCcalls, baseCcalls
+#include "../lib/lua/lstate.h"  // for L->nCcalls, baseCcalls
 
 lua_State* L;
 lua_State* Lt;
@@ -73,6 +73,15 @@ void lua_run_restore()
 	}
 }
 
+// get key ID of key name at arg, throw error if invalid
+static int lua_get_key_arg( lua_State * L, int narg )
+{
+    int k = script_keyid_by_name( luaL_checkstring( L, narg ) );
+    if(!k) 
+        luaL_error( L, "unknown key" );
+    return k;
+}
+
 #ifdef OPT_CURVES
 #include "curves.h"
 
@@ -109,13 +118,8 @@ static int luaCB_sleep( lua_State* L )
 // for press,release and click
 static int luaCB_keyfunc( lua_State* L )
 {
-  long k = keyid_by_name( luaL_checkstring( L, 1 ) );
-  if (k > 0 ) {
-    void* func = lua_touserdata( L, lua_upvalueindex(1) );
-    ((void(*)(long))func)( k );
-  }
-  else
-    luaL_error( L, "unknown key" );
+  void* func = lua_touserdata( L, lua_upvalueindex(1) );
+  ((void(*)(long))func)( lua_get_key_arg( L, 1 ) );
   return lua_yield( L, 0 );
 }
 
@@ -514,13 +518,13 @@ static int luaCB_wait_click( lua_State* L )
 
 static int luaCB_is_pressed( lua_State* L )
 {
-  lua_pushboolean( L, camera_is_pressed(luaL_checkstring( L, 1 )));
+  lua_pushboolean( L, script_key_is_pressed(lua_get_key_arg( L, 1 )));
   return 1;
 }
 
 static int luaCB_is_key( lua_State* L )
 {
-  lua_pushboolean( L, camera_is_clicked(luaL_checkstring( L, 1 )));
+  lua_pushboolean( L, script_key_is_clicked(lua_get_key_arg( L, 1 )));
   return 1;
 }
 
@@ -566,7 +570,7 @@ static int luaCB_md_detect_motion( lua_State* L )
   int msecs_before_trigger = (luaL_optnumber(L,16,0));
   if(md_init_motion_detector(
     columns, rows, pixel_measure_mode, detection_timeout, 
-    measure_interval, threshold, draw_grid, 0,
+    measure_interval, threshold, draw_grid,
     clipping_region_mode,
     clipping_region_column1, clipping_region_row1,
     clipping_region_column2, clipping_region_row2,

core/script.c
@@ -677,100 +677,62 @@ long script_start_ptp( char *script , int keep_result )
 }
 #endif
 
-#ifndef UBASIC_TEST
-int camera_is_pressed(const char *s)
+int script_key_is_pressed(int k)
 {
-    long k = keyid_by_name(s);
-    if (k==0xFF) return get_usb_power(1);
-    if (k > 0) {
-        return (kbd_is_key_pressed(k));
-    } else {
-#ifdef OPT_LUA
-        if (is_lua()) {
-            luaL_error( L, "unknown key" );
-        } else
-#endif
-        {
-#ifdef OPT_UBASIC
-            ubasic_error = UBASIC_E_UNK_KEY;
-#endif
-        }
-    }
+    if (k==0xFF)
+        return get_usb_power(1);
+    if (k > 0)
+        return kbd_is_key_pressed(k);
     return 0;
 }
 
-int camera_is_clicked(const char *s)
+int script_key_is_clicked(int k)
 {
-    long k = keyid_by_name(s);
-    if (k==0xFF) return get_usb_power(1);
-    if (k > 0) {
+    if (k==0xFF)
+        return get_usb_power(1);
+    if (k > 0)
         return (kbd_last_clicked == k);
-    } else {
-#ifdef OPT_LUA
-        if (is_lua()) {
-            luaL_error( L, "unknown key" );
-        } else
-#endif
-        {
-#ifdef OPT_UBASIC
-            ubasic_error = UBASIC_E_UNK_KEY;
-#endif
-        }
-    }
     return 0;
 }
 
-#ifdef OPT_UBASIC
-void camera_press(const char *s)
-{
-    // For Lua, luaCB_keyfunc handles this command.
+static const struct Keynames {
+    int keyid;
+    char *keyname;
+} keynames[] = {
+    { KEY_UP,           "up"         },
+    { KEY_DOWN,         "down"       },
+    { KEY_LEFT,         "left"       },
+    { KEY_RIGHT,        "right"      },
+    { KEY_SET,          "set"        },
+    { KEY_SHOOT_HALF,   "shoot_half" },
+    { KEY_SHOOT_FULL,   "shoot_full" },
+    { KEY_ZOOM_IN,      "zoom_in"    },
+    { KEY_ZOOM_OUT,     "zoom_out"   },
+    { KEY_MENU,         "menu"       },
+    { KEY_DISPLAY,      "display"    },
+    { KEY_PRINT,        "print"      },
+    { KEY_ERASE,        "erase"      },
+    { KEY_ISO,          "iso"        },
+    { KEY_FLASH,        "flash"      },
+    { KEY_MF,           "mf"         },
+    { KEY_MACRO,        "macro"      },
+    { KEY_VIDEO,        "video"      },
+    { KEY_TIMER,        "timer"      },
+    { KEY_EXPO_CORR,    "expo_corr"  },
+    { KEY_MICROPHONE,   "fe"         },
+    { KEY_ZOOM_ASSIST,  "zoom_assist"},
+    { KEY_AE_LOCK,      "ae_lock"    },
+    { KEY_METERING,     "metering_mode"},
+    { 0xFF,             "remote"     },
+    { 0xFFFF,           "no_key"     },
+};
 
-    long k = keyid_by_name(s);
-    if (k > 0) {
-        action_push_press(k);
-    } else {
-        ubasic_error = UBASIC_E_UNK_KEY;
-    }
+int script_keyid_by_name (const char *n)
+{
+    int i;
+    for (i=0;i<sizeof(keynames)/sizeof(keynames[0]);i++)
+    if (strcmp(keynames[i].keyname,n) == 0)
+        return keynames[i].keyid;
+    return 0;
 }
 
-void camera_release(const char *s)
-{
-    // For Lua, luaCB_keyfunc handles this command.
-
-    long k = keyid_by_name(s);
-    if (k > 0) {
-        action_push_release(k);
-    } else {
-        ubasic_error = UBASIC_E_UNK_KEY;
-    }
-}
-
-void camera_click(const char *s)
-{
-    // For Lua, luaCB_keyfunc handles this command.
-
-    long k = keyid_by_name(s);
-    if (k > 0) {
-        action_push_click(k);
-    } else {
-        ubasic_error = UBASIC_E_UNK_KEY;
-    }
-}
-
-void camera_shoot()
-{
-    action_push(AS_SHOOT);
-}
-
-void camera_sleep(long v)
-{
-    action_push_delay(v);
-}
-
-void camera_wait_click(int t)
-{
-    action_wait_for_click(t);
-}
-#endif
-
-#endif

include/script.h
@@ -37,6 +37,9 @@ int script_is_running();
 void script_end();
 long script_start_gui( int autostart );
 
+int script_key_is_pressed( int keyid );
+int script_key_is_clicked( int keyid );
+int script_keyid_by_name( const char *name );
 #ifdef OPT_LUA
 long script_start_ptp( char *script , int keep_result );
 #endif

lib/ubasic/camera_functions.c
@@ -8,40 +8,51 @@ int shot_histogram_enabled=0;
 int state_kbd_script_run=1;
 int zoom_points = 3;
 #define MODE_REC                0x0100
-
-void camera_press(const char *s)
-{
-    printf("*** button press '%s' ***\n",s);
+#define AS_SHOOT 1
+int script_keyid_by_name(const char *s) {
+	return 1;
 }
 
-void camera_release(const char *s)
-{
-    printf("*** button release '%s' ***\n",s);
+void console_set_autoredraw(int n) {
+    printf("*** console_set_auto_redraw %d ***\n",n);
 }
 
-void camera_click(const char *s)
+void action_push_press(int k)
 {
-    printf("*** button click '%s' ***\n",s);
+    printf("*** button press %d ***\n",k);
 }
 
-void camera_sleep(int v)
+void action_push_release(int k)
+{
+    printf("*** button release %d ***\n",k);
+}
+
+void action_push_click(int k)
+{
+    printf("*** button click %d ***\n",k);
+}
+
+void action_push_delay(int v)
 {
     printf("*** sleep %d ***\n",v);
 }
 
-void camera_shoot()
+void action_push(int id)
 {
-    printf("*** shoot ***\n");
+	if(id == AS_SHOOT)
+		printf("*** shoot ***\n");
+	else
+		printf("*** action_push(%d) ***\n",id);
 }
 
-void camera_wait_click(int t)
+void action_wait_for_click(int t)
 {
     printf("*** wait_click %d ***\n", t);
 }
 
-int camera_is_clicked(const char *s)
+int script_key_is_clicked(int k)
 {
-    printf("*** is_key '%s' ***\n", s);
+    printf("*** is_clicked %d ***\n", k);
     return 1;
 }
 
@@ -136,11 +147,9 @@ void console_set_layout(int x1, int y1, int x2, int y2) {
     printf(">>> set console layout to %d %d %d %d\n", x1,y1,x2,y2);
 }
 
-/*
 void script_console_set_autoredraw(int value) {
     printf(">>> set console auto_redraw to %d\n", value);
 }
-*/
 
 void console_redraw() {
     printf("*** console redraw ***\n");
@@ -440,9 +449,9 @@ int get_exposure_counter(void)
 }
 
 
-int camera_is_pressed(const char *v) 
+int script_key_is_pressed(const char *v) 
 {
-    printf("*** camera_is_pressed %s ***\n", v);
+    printf("*** script_key_is_pressed %s ***\n", v);
 	return 0;
 }
 

lib/ubasic/camera_functions.h
@@ -1,10 +1,14 @@
+/*
+header for building ubasic_test.exe,
+contains prototypes for functions that would be
+inconvenient to get from real header files
+see dummy functions in camera_functions.c 
+*/
+int script_key_is_clicked(int keyid);
+int script_key_is_pressed(int keyid);
 
-void camera_press(const char *s);
-void camera_release(const char *s);
-void camera_wait_click(int timeout);
-int camera_is_pressed(const char *s);
-int camera_is_clicked(const char *s);
-void camera_click(const char *s);
+int camera_is_pressed(int i);
+int camera_is_clicked(int i);
 void camera_sleep(long v);
 void camera_shoot();
 int md_detect_motion(void);

commit 1b8870a7acf865f053fd82a4819727bd277b67b6

fix ubasic click, press and release, broken in r1036
git-svn-id: https://subversion.assembla.com/svn/chdk/trunk@1042 6794e30b-3f2a-0410-a806-a2bbca1c07ff

lib/ubasic/ubasic.c
@@ -1433,7 +1433,9 @@ end_statement(void)
 static void
 click_statement(void)
 {
-  int k = ubasic_get_key_arg();
+  int k;
+  accept(TOKENIZER_CLICK);
+  k = ubasic_get_key_arg();
   if (k > 0)
     action_push_click(k);
 
@@ -1444,7 +1446,9 @@ click_statement(void)
 static void
 press_statement(void)
 {
-  int k = ubasic_get_key_arg();
+  int k;
+  accept(TOKENIZER_PRESS);
+  k = ubasic_get_key_arg();
   if (k > 0)
     action_push_press(k);
   DEBUG_PRINTF("End of press\n");
@@ -1454,7 +1458,9 @@ press_statement(void)
 static void
 release_statement(void)
 {
-  int k = ubasic_get_key_arg();
+  int k;
+  accept(TOKENIZER_RELEASE);
+  k = ubasic_get_key_arg();
   if (k > 0)
     action_push_release(k);
   DEBUG_PRINTF("End of release\n");